1b15d80f65fefc26e5972a8092addff714c63590
TriKhai <lytrikhaikg2021@gmail.com>
2024-04-09 22:59:05 +0700

    tsp: branch and bound

---
+++ TH/buoi3/README.md
@@ -0,0 +1,33 @@
+# TSP - BRANCH AND BOUND
+
+1. Declaration
+```mermaid
+graph LR
+A[Graph] --> B[Edge A_2chieu] 
+A --> C[int n]
+B --> D[DoDai]
+B --> E[dau]
+B --> F[cuoi]
+B --> G[daDung]
+```
+
+1. BranchAndBound
+The solve function takes a graph G, an index i, the current node current, the partial cost p_cost, the pointer to the final cost final_cost, and an array of edges x.
+
+- Initialize n to the number of vertices in the graph.
+- Iterate over all possible next nodes (next) from the current node.
+- Check if current is not equal to next and if the edge between them (G->E[current][next]) is not marked and if adding this edge does not create a cycle in the partial solution x.
+- If the above conditions are met:
+**a.** Calculate the cost of the current partial solution by adding the length of the edge from current to next to the partial cost p_cost.
+**b.** Calculate a lower bound estimate lbound for the remaining cost using the lowerBound function.
+**c.** If this lower bound is less than the current final cost, proceed to explore this path further.
+**d.** Update the partial solution x with the chosen edge (G->E[current][next]).
+**e.** Mark the chosen edge and its reverse to avoid revisiting them.
+**f.** If i indicates that all edges have been considered (i == n-2), update the final result if the current cost is better.
+**g.** Otherwise, recursively call the solve function with updated parameters to explore further.
+After exploring all possible paths from the current node, unmark the chosen edge and its reverse for backtracking.
+
+2. Funtions
+- isCircle()
+- lowerBound()
+- updateResult()
--- TH/buoi3/tsp.cpp
+++ TH/buoi3/tsp.cpp
@@ -1,109 +1,141 @@
 #include <stdio.h>
-#define size 30
-#define CMIN 3.40282e+38
+#include <malloc.h>
+#define MAX_N 100
+#define MIN_F 3.4e38
 
 typedef struct {
-	float doDai;
-	int dau, cuoi, daDung;
-} Canh;
+    float length;
+    int first, last, mark;
+} Edge;
 
+typedef struct {
+    int n;
+    Edge E[MAX_N][MAX_N];
+} Graph;
 
-int ReadData(Canh arr[][size], int *soThanhPho) {
-	FILE *f;
-	f = fopen("tsp.txt", "r");
-	fscanf(f, "%d", soThanhPho);
-	float tmp;
-	for(int i=1; i<=*soThanhPho; i++) {
-		for(int j=1; j<=*soThanhPho; j++) {
-			arr[i][j].dau = i;
-			arr[i][j].cuoi = j;
-			fscanf(f, "%f", &arr[i][j].doDai);
-			arr[i][j].daDung = 0;
-		}
-	}
-	fclose(f);
+void readFile(Graph *G){
+    FILE *f = fopen("TSP1.txt", "r");
+    fscanf(f,"%d", &G->n);
+    for(int i = 0; i < G->n; i++){
+        for(int j = 0; j < G->n; j++){
+            fscanf(f, "%f", &G->E[i][j].length);
+            G->E[i][j].first = i;
+            G->E[i][j].last  = j;
+            G->E[i][j].mark  = 0;
+        }
+    }
+    fclose(f);
 }
 
-void printData(Canh arr[][size], int soThanhPho) {
-	for(int i=1; i<=soThanhPho; i++) {
-		for(int j=1; j<=soThanhPho; j++) {
-			printf("%c%c=%-8.2f", arr[i][j].dau+64,
-			 arr[i][j].cuoi+64, arr[i][j].doDai);
-		}
-		printf("\n");
-	}
+void printGraph(Graph *G){
+    printf("Ma tran trong so cua do thi:\n");
+    for(int i = 0; i < G->n; i++){
+        for(int j = 0; j < G->n; j++){
+            printf(" %c%c =%5.2f  ", G->E[i][j].first+97, G->E[i][j].last+97, G->E[i][j].length);
+        }
+        printf("\n");
+    }
 }
 
-void reset(Canh arr[][size], int soThanhPho) {
-	for(int i=1; i<=soThanhPho; i++) {
-		for(int j=1; j<=soThanhPho; j++) {
-			arr[i][j].daDung = 0;
-		}
-	}
+void printResult(Edge x[], int n){
+    printf("Phuong an tim duoc:\n");
+    for(int i = 0; i < n; i++){
+        printf("  Canh %c%c = %5.2f\n", x[i].first+97, x[i].last+97, x[i].length);        
+    }
+    float sum = 0;
+    printf("Chu trinh:\n  ");
+    for(int i = 0; i < n; i++){
+        printf("%c -> ", x[i].first+97);
+        sum += x[i].length;
+    }
+    printf("%c\n", x[0].first+97);
+    printf("Tong do dai cac canh cua chu trinh = %5.2f\n\n", sum);
 }
 
-float canhNhoNhat(Canh arr[][size], int soThanhPho) {
-	float Cmin = CMIN;
-	for(int i=1; i<=soThanhPho; i++) {
-		for(int j=1; j<=soThanhPho; j++) {
-			if(i!=j && !arr[i][j].daDung && arr[i][j].doDai<Cmin) {
-				Cmin = arr[i][j].doDai;
-			}
-		}
-	}
-	return Cmin;
+int isCycle(Edge x[], int n, int next){
+    for(int i = 0; i < n; i++){
+        if(next == x[i].first)
+            return 1;
+    }
+    return 0;
 }
 
-float canDuoi(Canh arr[][size], float TGT, int soThanhPho, int bacCuaNut) {
-	return TGT+(soThanhPho-bacCuaNut)*canhNhoNhat(arr, soThanhPho);
+float lowerBound(Graph *G, float cost, int level){
+    float min_Edge = MIN_F;
+    for(int i = 0; i < G->n; i++){
+        for(int j = 0; j < G->n; j++){
+            if(i != j && !G->E[i][j].mark && G->E[i][j].length < min_Edge)
+                min_Edge = G->E[i][j].length;
+        }
+    }
+    return cost+(G->n - level)*min_Edge;
 }
 
-int kiemTraChuTrinh(Canh x[], int k, int keTiep) {
-	int coCT=0, i=0;
-	while(i<k && !coCT) {
-		if(keTiep == x[i].dau) coCT=1;
-		else i++;
-	}
-	return coCT;
+Edge result[MAX_N];
+void updateResult(Graph *G, float cost, float *final_cost, Edge x[]){
+    int n = G->n;
+    x[n-1] = G->E[x[n-2].last][x[0].first];
+    cost += x[n-1].length;
+    if(cost < *final_cost){
+        *final_cost = cost;
+        for(int i = 0; i < n; i++){
+            result[i] = x[i];
+        }
+    }
 }
 
-void CapNhatPATNTT(Canh arr[][size], int soThanhPho, float TGT, float *GiaNNTT, Canh x[], Canh PA[]) {
-	x[soThanhPho]=arr[soThanhPho-1][x[1].dau];
-	TGT = TGT+x[soThanhPho].doDai;
-	if(*GiaNNTT>TGT) {
-		*GiaNNTT = TGT;
-		for(int i=1; i<=soThanhPho; i++) {
-			PA[i]=x[i];
-		}
-	}
+void BranchAndBound(Graph *G, int i, int current, float p_cost, float *final_cost, Edge x[]){    
+    int n = G->n;
+    for(int next = 0; next < n; next++){
+        if(current != next && !G->E[current][next].mark && !isCycle(x, i, next)){
+            float cost, lbound;
+            cost = p_cost + G->E[current][next].length;
+            lbound = lowerBound(G, cost, i+1);
+            if(lbound < *final_cost){
+                x[i] = G->E[current][next];
+                G->E[current][next].mark = 1;
+                G->E[next][current].mark = 1;
+                if(i == n-2)
+                    updateResult(G, cost, final_cost, x);
+                else
+                    BranchAndBound(G, i+1, next, cost, final_cost, x);
+            }
+            G->E[current][next].mark = 0;
+            G->E[next][current].mark = 0;
+        }
+    }
 }
 
-void NhanhCan(Canh arr[][size], int soThanhPho, int bacCuaNut, float *TGT, float *CD, float *GiaNNTT, Canh x[], Canh PA[]) {
-	int j;//dinh ke tiep
-	for(j=1; j<=soThanhPho; j++) {
-		*TGT = *TGT + arr[dau][j].doDai;
-		*CD = canDuoi(arr, *TGT, soThanhPho, bacCuaNut+1);
-		if(*CD < *GiaNNTT) {
-			x[bacCuaNut]=arr[dau][j];
-			arr[dau][j].daDung=1;
-			arr[j][dau].daDung=1;
-			if(bacCuaNut == soThanhPho-2) {
-				CapNhatPATNTT()
-			} else {
-				NhanhCan(arr, soThanhPho, bacCuaNut+1, j, TGT, CD, GiaNNTT, x, PA);
-			}
-		} 
-		*TGT = *TGT = arr[dau][j].doDai;
-		arr[dau][j].daDung=0;
-		arr[j][dau].daDung=0;
-	}
+void reset(Graph *G){
+    for(int i = 0; i < G->n; i++){
+        for(int j = 0; j < G->n; j++){
+            G->E[i][j].mark = 0;
+        }
+    }
 }
 
-int main() {
-	Canh arr[size][size];
-	int soThanhPho;
-	ReadData(arr, &soThanhPho);
-	printData(arr, soThanhPho);
-	
-	return 0;
+int main(){
+    Graph G;
+    readFile(&G);    
+    printf("TSP su dung thuat toan NHANH CAN:\n\n");
+    Edge x[MAX_N];    
+    while(1){
+        printGraph(&G);
+        reset(&G);
+        float final_cost = MIN_F;
+        char s;
+        do {
+            printf("Chon thanh pho xuat phat (a,.., e): ");
+            scanf("\n%c", &s);
+        } while (s < 'a' || s > 'e');
+        BranchAndBound(&G, 0, s-97, 0, &final_cost, x);
+        printResult(result, G.n);        
+        do{
+            printf("Tiep tuc (Y/N)? ");
+            scanf("\n%c", &s);
+            if(s == 'n' || s == 'N')
+                return 0;
+        } while(s != 'y' && s != 'Y');
+     
+    }    
 }
